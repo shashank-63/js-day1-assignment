6911ef9a5383624bd0248ac6506fc756a1440d86
shashank-63 <amanmishra2757@gmail.com>
2026-02-24 05:11:08 +0000

    no change

---
+++ my-react-app/src/Employee.jsx
@@ -0,0 +1,6 @@
+export function Employee()
+{
+    return(
+        <h1>this is new component for employee</h1>
+    )
+}
---
+++ my-react-app/src/Resume.jsx
@@ -0,0 +1,6 @@
+export function Resume()
+{
+ return(
+        </><h1>MY NAME IS-SHASHANK MISHRA</h1><H1>I am parsuing my bachalor of technology in computer science engineering</H1><h1>roll no -2300100100364</h1>
+    )
+}
